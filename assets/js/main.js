885b5fa98072cc2e79bd3760a2eb0fa3907f5722
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 14:44:47 +0100

    wirte

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,20 @@ document.write(myText)
 document.write(`<h2>${myText}</h2>`)
 
 document.getElementById("antwort").innerHTML = myText
+
+let vorname = "Horst"
+let nachname = "Hubert"
+console.log(vorname+ " " + nachname)
+
+document.write(vorname + " " + nachname)
+
+document.write(`<h2>${ vorname + " " + nachname} hallo ${nachname} </h2>`)
+
+document.getElementById("idVorname").innerHTML = vorname + " " + nachname
+
+
+function myFunction(){
+    document.write(myText)
+    console.log("Button wurde gedrückt: Inhalt = " + myText)
+}
+console.log(myText)
